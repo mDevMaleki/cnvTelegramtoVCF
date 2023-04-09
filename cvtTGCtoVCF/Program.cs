ad3b444e01da6a5ebc125d5fcf22c266022d5e80
mdevmaleki <mdevmaleki@gmail.com>
2023-04-09 14:52:34 +0430

    Initial Commit

---
+++ cvtTGCtoVCF/Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace cvtTGCtoVCF
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText(@"C:\Users\administrator\Desktop\result.vcf", "");
+            if (args.Length != 0)
+                Console.WriteLine("help :::: cvtTGCtoVCF ./telgramresult.json .");
+            else
+            {
+
+                Rootobject telegramContacts = JsonConvert.DeserializeObject<Rootobject>(File.ReadAllText(@"C:\Users\administrator\Desktop\result.json"));
+                int index = 0;
+                foreach (TelegramContact item in telegramContacts.List)
+                {
+                    var text = $@"BEGIN:VCARD
+VERSION:2.1
+N:{item.first_name};{item.last_name};;;
+FN:{item.first_name} {item.last_name}
+TEL;CELL;PREF:{item.phone_number.Replace(" ","")}
+END:VCARD";
+                    if(telegramContacts.List.Count > ++index)
+                        File.AppendAllText(@"C:\Users\administrator\Desktop\result.vcf", text + "\r");
+                    else
+                        File.AppendAllText(@"C:\Users\administrator\Desktop\result.vcf", text );
+
+                }
+                //// Console.WriteLine(args[0]);
+                // string result = "";
+                // using (FileStream fs = File.Open(, FileMode.Open))
+                // {
+                //     byte[] b = new byte[1024];
+                //     UTF8Encoding temp = new UTF8Encoding(true);
+
+                //     while (fs.Read(b, 0, b.Length) > 0)
+                //     {
+                //         result += temp.GetString(b);
+                //     }
+
+                // }
+
+
+
+
+            }
+        }
+    }
+}
---
+++ cvtTGCtoVCF/TelegramContact.cs
@@ -0,0 +1,14 @@
+﻿namespace cvtTGCtoVCF;
+public class Rootobject
+{
+    public List<TelegramContact> List { get; set; }
+}
+
+public class TelegramContact
+{
+    public string first_name { get; set; }
+    public string last_name { get; set; }
+    public string phone_number { get; set; }
+    public DateTime date { get; set; }
+    public string date_unixtime { get; set; }
+}
